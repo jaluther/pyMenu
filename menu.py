a43c50057cd73b514892a2d6767ab9490984d9e0
iAutomate <james.luther@gmail.com>
2019-03-13 09:53:04 +0000

    tidy up code

--- menu.py
+++ menu.py
@@ -2,6 +2,8 @@
 import os,time
 import yaml,json
 
+debug=False
+
 # Build menu data from YAML file
 with open('devices.yml', 'r') as stream:
   menuDict = yaml.load(stream)
@@ -10,20 +12,23 @@ menuList = sorted(menuDict.keys())
 for i in menuList:
   print "for item: %s, ssh to: %s" % (i, menuDict[i]['conn'])
 
+def printMenu(menuList):
+  # clear screen 
+  os.system('clear')
+  # Print header
+  print '#'*30
+  print '# SSH Menu'
+  print '#'*30
+  # created numbered list (increment to start at 1)
+  for i in menuList:
+    print '%s\t%s' % (menuList.index(i)+1, i)
+  # print trailer
+  print ''
+  print 'q\tQuit'
+
 def main():
   while True:
-    # clear screen at start of every loop
-    os.system('clear')
-    # Print header
-    print '#'*30
-    print '# SSH Menu'
-    print '#'*30
-    # created numbered list (increment to start at 1)
-    for i in menuList:
-      print '%s\t%s' % (menuList.index(i)+1, i)
-    # print trailer
-    print ''
-    print 'q\tQuit'
+    printMenu(menuList)
     # get chosen input 
     choice = raw_input(">> ")
     try:
@@ -33,19 +38,20 @@ def main():
       # prepare vars (decrement by 1 to match dict keys)
       choice = int(choice)
       choice -=1
-      # if input below 0 skip loop (pass) - list behavour undesirable
+      # if input below 0 skip loop (raise) - list behavour undesirable
       if choice < 0:
         raise Exception('number out of range')
+
       # SSH to chosen device
       chosenDevice = menuList[choice]
-      print "for item: %s, Run: 'ssh %s'" % (chosenDevice, menuDict[chosenDevice]['conn'])
+      print "CONNECT: 'ssh %s'" % (menuDict[chosenDevice]['conn'])
       os.system('ssh ' + menuDict[chosenDevice]['conn'])
     except Exception, e:
-      # for DEBUG, uncomment
-      #print 'Exception caught: %s' % e
+      if debug:
+        print 'Exception caught: %s' % e
       pass
-    # for DEBUG, uncomment
-    #time.sleep(1)
+    if debug:
+      time.sleep(1)
 
 if __name__ == "__main__":
   main()
